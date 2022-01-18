91b34a79015e38667331dc4b189eceb41d7817ef
ISDementyev <65086972+ISDementyev@users.noreply.github.com>
2022-01-17 21:46:28 -0500

    Removing some redundancies

--- interfaces.py
+++ interfaces.py
@@ -364,11 +364,11 @@ class omm:
             printRecord("Beginning to iterate through chains...\n")
 
             for row in self.angles_to_constrain:
-                aa_id, phi, psi, chain_id = row[0], row[1], row[2], row[3]
-                aa_id, phi, psi, chain_id = int(aa_id), float(phi), float(psi), int(chain_id)
+                aa_id, phi, psi = int(row[0]), float(row[1]), float(row[2])
+                # aa_id, phi, psi = int(aa_id), float(phi), float(psi)
 
-                printRecord(f"aa_id = {aa_id}, phi = {phi}, psi = {psi}, chain_id = {chain_id}")
-                printRecord("Printing first 5 atoms in topology.atoms()...")
+                # printRecord(f"aa_id = {aa_id}, phi = {phi}, psi = {psi}, chain_id = {chain_id}")
+                # printRecord("Printing first 5 atoms in topology.atoms()...")
 
                 # first5 = [atom for atom in self.topology.atoms()]
                 # first5 = first5[:5]
@@ -376,16 +376,13 @@ class omm:
                 # for atom in first5:
                 # printRecord(f"Atom name={atom.name}, Atom residue chain index = {atom.residue.chain.index}, Atom residue index = {atom.residue.index}")
 
-                self.da_atoms = [atom for atom in self.topology.atoms() if
-                                 atom.residue.chain.index == chain_id and atom.name in {'N', 'CA',
-                                                                                        'C'} and atom.residue.index in {
-                                     aa_id, aa_id - 1}]
+                self.da_atoms = [atom for atom in self.topology.atoms() if atom.name in {'N', 'CA', 'C'} and atom.residue.index in {aa_id, aa_id - 1}]
 
                 printRecord("Identified da_atoms.\n")
 
                 # aa_id - 1 is included to account for the atoms in the previous residue being part of the current residue's dihedrals
 
-                printRecord("da_atoms length = " + str(len(self.da_atoms)))  # returns 0 for some reason
+                printRecord(f"da_atoms length = {len(self.da_atoms)}")  # returns 0 for some reason
 
                 for i in range(len(self.da_atoms) - 3):
                     self.tup = tuple([atom.name for atom in self.da_atoms[i:i + 4]])
@@ -407,7 +404,7 @@ class omm:
                                                   self.tupIndex[1],
                                                   self.tupIndex[2],
                                                   self.tupIndex[3], (psi * rad_conv,) * radians)
-                            printRecord("Successfully added a phi torsion restraint.\n")
+                            printRecord("Successfully added a psi torsion restraint.\n")
 
             self.system.addForce(self.force)
             printRecord("Successfully added the force.\n")
